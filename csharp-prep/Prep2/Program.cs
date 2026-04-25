b581501732be26fad2b18f2e9f16fc43fd17f11d
Joanna Hanks <han21015@byui.edu>
2026-04-25 14:45:54 -0600

    Started and finished the prep assignment #2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int number = int.Parse(answer);
+
+        Console.WriteLine("");
+
+        string grade = "";
+        string pass = "";
+
+        if (number >= 90)
+        {
+            grade = "A";
+        }
+        else if (number >= 80)
+        {
+            grade = "B";
+        }
+        else if (number >= 70)
+        {
+            grade = "C";
+        }
+        else if (number >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+
+        if (grade == "A" || grade == "B" || grade == "C")
+        {
+            pass = ("Congragulations! You have passed this course!");
+        }
+        else
+        {
+            pass = ("You did not pass this course :O");
+        }
+
+
+        Console.WriteLine($"You got a {grade}!");
+        Console.WriteLine($"{pass}");
+        Console.WriteLine("");
     }
 }
