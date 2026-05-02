804a067b5dc19010b0ecad20c1380deb8ad5cd0f
Joanna Hanks <han21015@byui.edu>
2026-05-02 15:13:22 -0600

    Started and finished prep assignment 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int answer = randomGenerator.Next(1, 101);
+        
+
+        int attempt;
+
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            string number = Console.ReadLine();
+            attempt = int.Parse(number);
+
+            if (attempt < answer)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (attempt > answer)
+            {
+                Console.WriteLine("Lower");
+            }
+        } while (attempt != answer);
+
+        Console.WriteLine("You guessed it!");
     }
+
+
 }
